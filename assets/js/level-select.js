6386f250611c5b20eeb38b5f657ea7ec1b6bfe67
Tomtom67170 <berstheim67@gmail.com>
2025-12-05 05:18:58 +0100

    Fix conneries de Victor

--- assets/js/level-select.js
+++ assets/js/level-select.js
@@ -526,11 +526,11 @@ const progressbar = document.getElementById("progress");
 if (element && progressbar) {
   function handleMouseOver() {
     const score = getCookie("score") || 0;
-    progressbar.style.width = 25 * score + "%";
+    progressbar.style.width = 20 * score + "%";
   }
   function handleMouseOut() {
     const score = getCookie("score") || 0;
-    progressbar.style.width = 25 * score + "%";
+    progressbar.style.width = 20 * score + "%";
   }
   element.addEventListener("mouseover", handleMouseOver);
   element.addEventListener("mouseout", handleMouseOut);
@@ -538,6 +538,30 @@ if (element && progressbar) {
 if(element && progressbar) {
     function handleMouseOver() {
       const score = getCookie('score') || 0; 
+      progressbar.style.width = (20 * score)+'%';
+      const woman = document.getElementById('women');
+      const decat = document.getElementById('decat');
+      const cve = document.getElementById('cve');
+      const ergo = document.getElementById('ergo');
+      const linux = document.getElementById('linux');
+      if (getCookie('woman') == "1"){
+        woman.src = "assets/images/medals/femme.png";
+      }
+      if (getCookie('decat') == "1"){
+        decat.src = "assets/images/medals/decathlon.png";
+      }
+      if (getCookie('cve') == "1"){
+        cve.src = "assets/images/medals/peche.png";
+      }
+      if (getCookie('ergo') == "1"){
+        ergo.src = "assets/images/medals/brain.png";
+      }
+      if (getCookie('linux') == "1"){
+        linux.src = "assets/images/medals/linux.png";
+      }
+    }
+    function handleMouseOut() {
+      const score = getCookie('score') || 0;
       progressbar.style.width = (25 * score)+'%';
       const woman = document.getElementById('women');
       const decat = document.getElementById('decat');
@@ -557,27 +581,7 @@ if(element && progressbar) {
         ergo.src = "assets/images/medals/brain.png";
       }
       if (getCookie('linux') == "1"){
-        ergo.src = "assets/images/medals/linux.png";
-      }
-    }
-    function handleMouseOut() {
-      const score = getCookie('score') || 0;
-      progressbar.style.width = (25 * score)+'%';
-      const woman = document.getElementById('women');
-      const decat = document.getElementById('decat');
-      const cve = document.getElementById('cve');
-      const ergo = document.getElementById('ergo');
-      if (getCookie('woman') == "1"){
-        woman.src = "assets/images/medals/femme.png";
-      }
-      if (getCookie('decat') == "1"){
-        decat.src = "assets/images/medals/decathlon.png";
-      }
-      if (getCookie('cve') == "1"){
-        cve.src = "assets/images/medals/peche.png";
-      }
-      if (getCookie('ergo') == "1"){
-        ergo.src = "assets/images/medals/brain.png";
+        linux.src = "assets/images/medals/linux.png";
       }
     }
     element.addEventListener('mouseover', handleMouseOver);
